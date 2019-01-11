8787da729832794dcc7aa186a547175d77efcc2d
Randall Sánchez A <rsancheza24@gmail.com>
2019-01-11 00:00:18 -0600

    add header to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 
+import Header from './components/Header';
+
+import './assets/scss/index.scss';
+
 class App extends Component {
   render() {
     return (
       <Container className="App">
-        App
+        <Header />
       </Container>
     );
   }
